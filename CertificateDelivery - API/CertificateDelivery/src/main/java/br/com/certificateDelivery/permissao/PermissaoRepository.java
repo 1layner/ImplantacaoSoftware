e9e38b9506c9ec3f5d4764ed893516b2d5f4e793
Layner Rodrigues Costa <layner2008@hotmail.com>
2017-10-08 21:54:45 -0300

    'Commit da parte do projeto utilizado para fazer a cobertura de código'

---
+++ CertificateDelivery - API/CertificateDelivery/src/main/java/br/com/certificateDelivery/permissao/PermissaoRepository.java
@@ -0,0 +1,8 @@
+package br.com.certificateDelivery.permissao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PermissaoRepository extends JpaRepository<PermissaoEntity, Long> {
+	
+	
+}
